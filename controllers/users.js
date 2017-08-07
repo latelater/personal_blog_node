51be229adf63b3d40751eac275b22b7597893ac6
latelater <903430068@qq.com>
2017-08-07 11:10:43 +0800

    文章的数据字段

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,60 @@
 const User = require('../models/usersInfo').User;
+const Category = require('../models/categoryList').Category;
 const pinyin = require('pinyin');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 exports.create_user = function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
-    console.log(req.body);
-    console.log(`username: ${username}\npassword: ${password}`);
-
-    User.create({
-        username: username,
-        password: password
-    }, function (err, user) {
-            if (err) {
-                res.json({
-                    code: err.code,
-                    message: err.errmsg,
-                });
-            } else {
-                res.json({
-                    code: 200,
-                    message: "success",
-                });
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        User.create({
+            username: username,
+            password: hash
+        }, function (err, user) {
+                if (err) {
+                    res.json({
+                        code: err.code,
+                        message: err.errmsg,
+                    });
+                } else {
+                    res.json({
+                        code: 200,
+                        message: "success",
+                    });
+                }
             }
-        }
-    );
+        );
+    })  
 };
+
+
+// exports.create_user = function (req, res, next) {
+//     var username = req.body.username;
+//     var password = req.body.password;
+
+//     bcrypt.hash(password, saltRounds, function (err, hash) {
+//         console.log(hash);
+//         User.create({
+//                 username: username,
+//                 password: hash
+//             }, function (err, user) {
+//                 if (err) {
+//                     res.json({
+//                         code: err.code,
+//                         message: CodeMsg[err.code] || CodeMsg['500'],
+//                         data: err.message
+//                     });
+//                 } else {
+//                     res.json({
+//                         code: 200,
+//                         message: CodeMsg['200'],
+//                         data: user._id
+//                     });
+//                 }
+//             }
+//         );
+//     });
+
+// };
